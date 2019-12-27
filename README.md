99e7c53fe1f0b487c7201b2267f40482518152d5
Derek Wheelden <derek.wheelden@gmail.com>
2019-12-27 14:30:39 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Pallies Demo API
---
+++ server/manifest.js
@@ -0,0 +1,74 @@
+'use strict';
+
+const Dotenv = require('dotenv');
+const Confidence = require('confidence');
+const Toys = require('toys');
+const Config = require(`${__dirname}/.palliesrc`);
+
+// Pull .env into process.env
+Dotenv.config({ path: `${__dirname}/.env` });
+
+// Glue manifest as a confidence store
+module.exports = new Confidence.Store({
+    server: {
+        host: 'localhost',
+        port: {
+            $env: 'PORT',
+            $coerce: 'number',
+            $default: 3000
+        },
+        debug: {
+            $filter: { $env: 'NODE_ENV' },
+            $default: {
+                log: ['error'],
+                request: ['error']
+            },
+            production: {
+                request: ['implementation']
+            }
+        }
+    },
+    register: {
+        plugins: [
+            {
+                plugin: '../lib', // Main plugin
+                options: {}
+            },
+            {
+                plugin: '../node_modules/pallies/lib',
+                options: {
+                    isDev: {
+                        $filter: {
+                            $env: 'NODE_ENV'
+                        },
+                        $default: false,
+                        production: false,
+                        development: true
+                    },
+                    ...Config.pallies
+                }
+            },
+            {
+                plugin: {
+                    $filter: { $env: 'NODE_ENV' },
+                    $default: 'hpal-debug',
+                    production: Toys.noop
+                }
+            },
+            {
+                plugin: 'schwifty',
+                options: {
+                    $filter: { $env: 'NODE_ENV' },
+                    $default: {},
+                    $base: {
+                        migrateOnStart: true,
+                        knex: Config.knex
+                    },
+                    production: {
+                        migrateOnStart: false
+                    }
+                }
+            }
+        ]
+    }
+});
